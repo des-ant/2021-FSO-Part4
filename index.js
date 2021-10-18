b3ac6bcfc03ed2776cd163dbc78a0d9e26c7d0be
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-18 21:52:11 +1100

    Extracted handling of environment variables to utils/config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
+const config = require('./utils/config');
 
 const app = express();
 
@@ -28,8 +28,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
+mongoose.connect(config.MONGODB_URI);
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -49,7 +48,6 @@ app.post('/api/blogs', (request, response) => {
     });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
