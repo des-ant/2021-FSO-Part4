18a08523976b9ca0ab0a667634e277a95de311d8
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-19 11:00:46 +1100

    Updated totalLikes function to pass all tests -ex4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,14 +1,22 @@
+/* eslint-disable no-unused-vars, arrow-body-style */
 const dummy = (blogs) => {
   // Receives array of blog posts as parameter
   // Return value 1
   return 1;
 };
+/* eslint-enable no-unused-vars, arrow-body-style */
 
+/* eslint-disable arrow-body-style */
 const totalLikes = (blogs) => {
   // Receive list of blog posts as a parameter
   // Return total sum of likes in all of the blog posts
-  return 1;
+  const reducer = (sumOfLikes, blog) => {
+    return sumOfLikes + blog.likes;
+  };
+
+  return blogs.reduce(reducer, 0);
 };
+/* eslint-enable arrow-body-style */
 
 module.exports = {
   dummy,
