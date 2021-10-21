f5bc7df3a57b994690b77ffaa941a5a1857e5562
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-21 11:40:41 +1100

    Initialised database before tests, added test cases to confirm correct blogs are returned by GET to api/blogs

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,36 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog');
 
 const api = supertest(app);
+
+const initialBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
 
 // Makes an HTTP GET request to the /api/blogs url.
 // Verify that the blog list application returns
@@ -13,6 +41,21 @@ test('blogs are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/);
 }, 100000);
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs');
+
+  expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs');
+
+  const title = response.body.map((r) => r.title);
+  expect(title).toContain(
+    'Go To Statement Considered Harmful',
+  );
+});
 
 afterAll(() => {
   mongoose.connection.close();
