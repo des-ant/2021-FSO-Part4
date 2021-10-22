1659fa4873b11d8c2cb611873cb920e13fdbb49c
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-22 17:46:55 +1100

    Added restrictions to creating new users: min length 3 chars, username must be unique. Added unit tests -ex4.16

--- controllers/users.js
+++ controllers/users.js
@@ -7,8 +7,13 @@ usersRouter.get('/', async (request, response) => {
   response.json(users.map((user) => user.toJSON()));
 });
 
+// eslint-disable-next-line consistent-return
 usersRouter.post('/', async (request, response) => {
   const { body } = request;
+
+  if (body.password.length < 3) {
+    return response.status(400).json({ error: '`password` must be at least 3 characters' });
+  }
 
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(body.password, saltRounds);
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -215,6 +215,65 @@ describe('when there is initially one user in db', () => {
     const usernames = usersAtEnd.map((u) => u.username);
     expect(usernames).toContain(newUser.username);
   });
+
+  test('creation fails with invalid username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'na',
+      name: 'Username Tooshort',
+      password: 'usernametooshort',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
+
+  test('creation fails with invalid password', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'validusername',
+      name: 'Password Tooshort',
+      password: 'na',
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400);
+
+    expect(result.body.error).toContain('`password` must be at least 3 characters');
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    expect(result.body.error).toContain('`username` to be unique');
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
 });
 
 afterAll(() => {
